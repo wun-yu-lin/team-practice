bafcff6a76e0bdd19d616991051a32c88ba1eb76
Katlyn <katlyn880815@gmail.com>
2023-10-16 11:22:13 +0800

    team-member-17 Katlyn's first-version

---
+++ js/code-17.js
@@ -0,0 +1,5 @@
+window.onload() = function (){
+    const myElement = document.querySelector(".user-17");
+    myElement.textContent = "I'm number 17 developer";
+    myElement.classList.add("number-17");
+}
