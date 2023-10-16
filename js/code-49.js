58c5d294f49675932b5825e153269c1fa032a718
Brian-Wang0926 <aa0988402763@gmail.com>
2023-10-16 11:09:21 +0800

    push:user49

---
+++ js/code-49.js
@@ -0,0 +1,5 @@
+document.querySelector(".user-49").textContent = "Brian Wang";
+let btn = document.createElement("button");
+btn.textContent = "點我";
+btn.className="myBtn";
+document.querySelector(".user-49").appendChild(btn);
